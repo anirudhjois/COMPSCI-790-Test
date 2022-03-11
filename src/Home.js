d14c710d08f5c00ac7a33ac1ee2d724052ff09ac
ivenkateswarlu <ivenkateswarlu777@gmail.com>
2022-03-11 17:14:02 -0600

    Created a test example to see the reflected changes

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,11 @@ import { useState } from "react";
 
 const Home = () => {
     const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
 
     const handleClick = (e) => {
+        setName('luigi');
+        setAge(30);
         console.log('Hello NetRoomers', e);
     }
 
@@ -14,6 +17,7 @@ const Home = () => {
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click me</button>
             <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
             </div>
