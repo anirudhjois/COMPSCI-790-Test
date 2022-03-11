d7879eaf93ced0b07d6733fe8506d055902db178
Anirudh <anirudh.jois3@gmail.com>
2022-03-11 16:48:31 -0600

    Cleaning up App.js and removing unwanted code

--- src/App.js
+++ src/App.js
@@ -3,26 +3,12 @@ import Home from './Home';
 
 function App() {
   const title = 'Welcome to NetRoom';
-  const likes = 50;
-  const link = 'http://www.google.com';
 
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/* <p>{ person }</p> */}
-
-        {/* <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p> */}
-
-        <a href={link}>Google Site</a>
       </div>
     </div>
   );
