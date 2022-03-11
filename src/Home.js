ce55bc9c7a72980ed36e3be64cc89eefde0d9e77
Anirudh <anirudh.jois3@gmail.com>
2022-03-11 16:29:14 -0600

    Handling the click again using event object

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,15 @@ const Home = () => {
         console.log('Hello NetRoomers', e);
     }
 
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+      }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
             </div>
 
     );
