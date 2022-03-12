a25072654b47b3af58875014af4aa800fe64a9db
Tarique Shaikh <ttshaikh@uwm.edu>
2022-03-11 20:44:00 -0600

    Done few modifications for testing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to NetRoom';
-
   return (
     <div className="App">
       <Navbar />
--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,22 @@
 import { useState } from "react";
 
 const Home = () => {
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
-    const handleClick = (e) => {
-        setName('luigi');
-        setAge(30);
-        console.log('Hello NetRoomers', e);
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
-      }
-
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
-            </div>
-
-    );
+  return (
+    <div className="home">
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
+    </div>
+  );
 }
  
 export default Home;
