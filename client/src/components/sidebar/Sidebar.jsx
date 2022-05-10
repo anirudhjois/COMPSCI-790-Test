ab2713f930a3333eebc7e22b955e97e865c2b358
ivenkateswarlu <ivenkateswarlu777@gmail.com>
2022-05-09 22:44:19 -0500

    Sidebar src updated

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -18,7 +18,7 @@ export default function Sidebar() {
       <div className="sidebarItem">
         <span className="sidebarTitle">ABOUT ME</span>
         <img
-          src="https://i.pinimg.com/236x/1e/3f/58/1e3f587572a7a7b20bbf1828595a1786--holiday-party-themes-holiday-gift-guide.jpg"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQygdkrmLEAz8i3khlLufcBMtBIxoWAPNqtIQ&usqp=CAU"
           alt=""
         />
         <p>
